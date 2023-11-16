19b11159917eb968d0b12150ab7f96d46fb562aa
tanto <tantows001@gmail.com>
2023-11-16 09:21:08 +0700

    feat: praktik

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InfoController;
+use App\Http\Controllers\GreetController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/info', [InfoController::class, 'index'])->name('info');
+Route::get('/greet', [GreetController::class,'greet'])->name('greet');
