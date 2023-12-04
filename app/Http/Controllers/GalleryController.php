ab6183d98df4df10e728c110ff4faab56e700284
tanto <tantows001@gmail.com>
2023-12-04 19:08:44 +0700

    feat: post update

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -28,63 +28,6 @@ class GalleryController extends Controller
         return GalleryResource::collection($posts);
     }
 
-    /**
- * @OA\Post(
- *     path="/api/gallery",
- *     tags={"gallery"},
- *     summary="Create a new gallery item",
- *     description="Create a new gallery item with title, body, and image",
- *     operationId="createGalleryItem",
- *     @OA\RequestBody(
- *         required=true,
- *         description="Gallery item details",
- *         @OA\MediaType(
- *             mediaType="application/json",
- *             @OA\Schema(
- *                 @OA\Property(
- *                     property="title",
- *                     type="string",
- *                     description="Title of the gallery item"
- *                 ),
- *                 @OA\Property(
- *                     property="body",
- *                     type="string",
- *                     description="Body/content of the gallery item"
- *                 ),
- *                 @OA\Property(
- *                     property="image",
- *                     type="string",
- *                     format="url",
- *                     description="URL of the image for the gallery item"
- *                 ),
- *                 example={"title": "Sample Title", "body": "Sample Body", "image": "https://example.com/sample-image.jpg"}
- *             )
- *         )
- *     ),
- *     @OA\Response(
- *         response=201,
- *         description="Gallery item created successfully",
- *         @OA\JsonContent(
- *             @OA\Property(
- *                 property="message",
- *                 type="string",
- *                 example="Gallery item created successfully"
- *             ),
- *             @OA\Property(
- *                 property="data",
- *                 type="object",
- *                 ref="#/components/schemas/GalleryItem"  // Reference to a schema for the gallery item (you can define this separately)
- *             )
- *         )
- *     ),
- *     @OA\Response(
- *         response="default",
- *         description="Error occurred"
- *     )
- * )
- */
-    
-
     public function store(Request $request){
         $validatedData = $request->validate([
             "title"=> "required",
